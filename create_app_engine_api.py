20ffb3a2b80df1eed25da46fe6ff5df63999e9e5
nikolay <niki130396@gmail.com>
2022-01-17 09:20:48 +0200

    Connect to bigquery

--- create_app_engine_api.py
+++ create_app_engine_api.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+credentials = service_account.Credentials.from_service_account_file("/home/nikolay/PycharmProjects/Python-GCP/gcp-service-account-keys.json")
+client = bigquery.Client(credentials=credentials)
+
+dataset = client.get_dataset("dataset")
+table_id = f"{client.project}.{dataset.dataset_id}.students_table"
 
 
 app = FastAPI()
@@ -6,4 +14,19 @@ app = FastAPI()
 
 @app.get("/people")
 def get_people():
-    return {"people": []}
+    query = f"""
+        SELECT *
+        FROM {table_id}
+    """
+
+    query_job = client.query(query)
+
+    return {
+        "people":
+            [
+                {
+                    "name": row[0],
+                    "age": row[1]
+                } for row in query_job
+            ]
+    }
